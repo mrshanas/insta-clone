468db055dabdb9225e8f97e2faacde6a7b70ead3
mrshanas <nassibshaban345@gmail.com>
2022-02-24 03:14:50 +0300

    Refactor goal controllers

--- server/controllers/goals.js
+++ server/controllers/goals.js
@@ -2,14 +2,11 @@ import Goal from "../models/goalModel.js";
 
 export const displayGoals = (req, res) => {
   Goal.find({ author: req.user.id }, (err, goals) => {
-    if (!err) {
-      res.status(200).json({ goals });
-    } else {
-      console.log(err);
-      res.status(404).json({
-        message: "Not found",
-      });
-    }
+    !err
+      ? res.status(200).json({ goals })
+      : res.status(404).json({
+          message: "Not found",
+        });
   });
 };
 
@@ -18,42 +15,35 @@ export const createGoal = (req, res) => {
   goal.author = req.user.id;
 
   Goal.create(goal, (err, doc) => {
-    if (!err) {
-      res.status(201).json({ message: "Successfully created", doc });
-    } else {
-      console.log(err);
-      res.status(500).json({
-        message: "Internal server error",
-      });
-    }
+    !err
+      ? res.status(201).json({ message: "Successfully created", doc })
+      : res.status(500).json({
+          message: "Internal server error",
+        });
   });
 };
 
 export const displayGoal = (req, res) => {
   Goal.find({ author: req.user.id, _id: req.params.goalID }, (err, goal) => {
-    if (!err) {
-      res.status(200).json({
-        success: true,
-        goal,
-      });
-    } else {
-      res.status(404).json({
-        success: false,
-        message: "Goal not found",
-      });
-    }
+    !err
+      ? res.status(200).json({
+          success: true,
+          goal,
+        })
+      : res.status(404).json({
+          success: false,
+          message: "Goal not found",
+        });
   });
 };
 
 export const deleteGoal = (req, res) => {
   Goal.deleteOne({ author: req.user.id, _id: req.params.goalID }, (err) => {
-    if (!err) {
-      res.status(204);
-    } else {
-      res.status(404).json({
-        success: false,
-        message: "A goal with that id does not exist",
-      });
-    }
+    !err
+      ? res.status(204)
+      : res.status(404).json({
+          success: false,
+          message: "A goal with that id does not exist",
+        });
   });
 };
