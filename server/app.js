d90e7c3f750a982e9c4465a6fdd8fcbfc8a5f7f5
shanas <nassibshaban345@gmail.com>
2022-05-05 23:12:02 +0300

    dev: admin dashboard

--- server/app.js
+++ server/app.js
@@ -3,9 +3,26 @@ import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import cors from "cors";
+import AdminJs from "adminjs";
+import AdminExpress from "@adminjs/express";
+import AdminMongoose from "@adminjs/mongoose";
+import User from "./models/userModel.js";
+import Post from "./models/postModel.js";
+import Comment from "./models/comment.js";
 
 const app = express();
 
+AdminJs.registerAdapter(AdminMongoose);
+
+const adminJs = new AdminJs({
+  databases: [],
+  rootPath: "/admin",
+  resources: [Post, User, Comment],
+});
+
+const adminRouter = AdminExpress.buildRouter(adminJs);
+
+app.use(adminJs.options.rootPath, adminRouter);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
@@ -20,7 +37,7 @@ mongoose
   .then(() =>
     app.listen(PORT, () =>
       console.log(
-        `Server started on port ${PORT} and connected to the database`
+        `Server started on port ${PORT} and connected to the database and admin path is on /admin`
       )
     )
   )
--- server/models/postModel.js
+++ server/models/postModel.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import Comment from "./comment.js";
 
 const postSchema = new mongoose.Schema(
   {
